5f2d31708e450b201c1d6ad19613674d50ed6e2b
Jared Carlson <JaredC.Scholar@gmail.com>
2021-05-19 14:27:42 -0400

    Added masking function and related tests.

--- nd_emulator/mask.py
+++ nd_emulator/mask.py
@@ -1,117 +1,61 @@
-# import numpy as np
-#
-#
-# def get_extent(X, Y=None, already_log=False):
-#     """
-#     Returns the log10() of the extent of X and Y.
-#     :param X: (2d array or dict) meshgrid density array or table dict. If Y is not included it is assumed X
-#         is a dict.
-#     :param Y: (2d array) meshgrid temp array
-#     :param already_log: (bool) if X and Y are already in log space don't take log.
-#     :return: [lower_den, upper_den, lower_temp, upper_temp]
-#     """
-#     # if a table is the input unpack first
-#     if isinstance(X, dict):
-#         Y = X['temp']
-#         X = X['den']
-#     # Return extent and do log transform if needed
-#     if already_log:
-#         return np.array([X[0, 0], X[0, -1], Y[0, 0], Y[-1, 0]])
-#     else:
-#         return np.log10(np.array([X[0, 0], X[0, -1], Y[0, 0], Y[-1, 0]]))
-#
-def create_mask():
+import numpy as np
 
-    return
-#
-# class Mask:
-#
-#     def __init__(self, X, Y, x_range, y_range, log_transform_XY=True, require_square=True):
-#         """
-#         A mask for 2d data that has a corresponding X, Y 2d domain, such as generated from np.meshgrid.
-#         It is assumed that the points are on a regular square grid, i.e., all points are evenly spaced in x and y dims.
-#         :param X: (2d array)
-#         :param Y: (2d array)
-#         :param x_range: [x_low, x_high]
-#         :param y_range: [y_low, y_high]
-#         :param log_transform_XY: (bool) if x_range and y_range are in log domain but X and Y are not.
-#         """
-#         # transform and grab arrays
-#         if log_transform_XY:
-#             x_array = np.log10(X[0, :])
-#             y_array = np.log10(Y[:, 0])
-#         else:
-#             x_array = X[0, :]
-#             y_array = Y[:, 0]
-#
-#         # unpack ranges
-#         self.lb = x_range[0]
-#         self.rb = x_range[1]
-#         self.bb = y_range[0]
-#         self.tb = y_range[1]
-#
-#         # Find boundaries in X
-#         EPS = 10 ** -13
-#         SMALL = 10 ** -200
-#         # All of the complexity is to make sure that if the boundary or an entry in x or y is zero it is put in the right cell.
-#         self.l_i = next((i for i, x in enumerate(x_array) if
-#                          x + EPS * (abs(self.lb) + abs(self.rb)) >= self.lb * (1 - EPS * np.sign(x)) - SMALL), None)
-#         self.r_i = next((i for i, x in enumerate(x_array) if
-#                          x - EPS * (abs(self.rb) + abs(self.lb)) >= self.rb * (1 + EPS * np.sign(x)) + SMALL), None)
-#         self.b_i = next((i for i, y in enumerate(y_array) if
-#                          y + EPS * (abs(self.bb) + abs(self.tb)) >= self.bb * (1 - EPS * np.sign(y)) - SMALL), None)
-#         self.t_i = next((i for i, y in enumerate(y_array) if
-#                          y - EPS * (abs(self.tb) + abs(self.bb)) >= self.tb * (1 + EPS * np.sign(y)) + SMALL), None)
-#         # check for far right or top
-#         if self.r_i is None:
-#             self.r_i = len(x_array)
-#         if self.t_i is None:
-#             self.t_i = len(y_array)
-#         # Check that domain is square in index space.
-#         self.Nx = self.r_i - self.l_i
-#         self.Ny = self.t_i - self.b_i
-#         if require_square and self.r_i - self.l_i != self.t_i - self.b_i:
-#             raise RuntimeError(f"Masked region not square! {self.r_i - self.l_i} != {self.t_i - self.b_i}")
-#
-#     def __call__(self, X, flt=False, return_logical_mask=False):
-#         """
-#         return the masked portion of X. If X is a dict, a dictionary is returned where the domain and all vars have
-#         been masked.
-#         :param X: (2d array) or (dict) table
-#         :param flt: (bool) return flattened matrix
-#         :param return_logical_mask: (bool) return a logical mask of same shape as X
-#         :return: (2d array) a subset of X
-#         """
-#         if isinstance(X, dict):
-#             # assume it is a table
-#             table_new = {}
-#             table_new['den'] = X['den'][self.b_i:self.t_i, self.l_i:self.r_i]
-#             table_new['temp'] = X['temp'][self.b_i:self.t_i, self.l_i:self.r_i]
-#             table_new['Table_Values'] = {}
-#             for key in X['Table_Values'].keys():
-#                 table_new['Table_Values'][key] = X['Table_Values'][key][self.b_i:self.t_i, self.l_i:self.r_i]
-#             return table_new
-#
-#         if return_logical_mask:
-#             mask = np.zeros_like(X, dtype=np.bool_)
-#             mask[self.b_i:self.t_i, self.l_i:self.r_i] = True
-#             return mask
-#         if flt:
-#             return X[self.b_i:self.t_i, self.l_i:self.r_i].flatten()
-#         else:
-#             return X[self.b_i:self.t_i, self.l_i:self.r_i]
-#
-#
-# class IndexMask(Mask):
-#     def __init__(self, left_i, right_i, bottom_i, top_i):
-#         """
-#         A child of the Mask class that has the index boundaries as an input.
-#         :param left_i: the left index of the mask
-#         :param right_i: the right index of the mask
-#         :param bottom_i: the bottom index of the mask
-#         :param top_i: the top index of the mask
-#         """
-#         self.l_i = left_i
-#         self.r_i = right_i
-#         self.b_i = bottom_i
-#         self.t_i = top_i
+
+def create_mask(sub_domain, domain, dims, transforms, domain_rounding_type=None):
+    """
+            A mask for nd data. It is assumed that the data is evenly spaced after the transform has been done.
+            :param sub_domain: (2d-array) [[x0_lo, x0_hi], [x1_lo, x1_hi],... [xN_lo, xN_hi]] the upper and lower bound of each
+                dimension for the new domain. Should be contained inside full domain
+            :param domain: (2d-array) [[x0_lo, x0_hi], [x1_lo, x1_hi],... [xN_lo, xN_hi]] the upper and lower bound of each
+                dimension for the full domain.
+            :param dims: (list) the number of elements along each axis of the data matrix
+            :param transforms: (list) of transforms to perform to make domain an evenly spaced grid of points. Each entry
+                corresponds to the corresponding dimension.
+            :param domain_rounding_type: [None, 'expand', 'contract'] How to handle subdomains that do not line up with
+                points. expand will round the index to the extreme and contract will do the opposite.
+            """
+    EPS = 10 ** -10
+    # Make sure the sub-domain is within the original domain
+    assert (len(domain) == len(sub_domain) and len(domain) == len(transforms) and len(domain) == len(dims))
+    num_dims = len(transforms)
+    for i in range(num_dims):
+        assert (domain[i][0] <= sub_domain[i][0] and domain[i][1] >= sub_domain[i][1])
+
+    # do needed transforms
+    d = domain.copy()
+    d_sub = sub_domain.copy()
+    for i in range(num_dims):
+        if transforms[i] is None:
+            continue
+        elif transforms[i] == 'log':
+            assert (domain[i] > 0)
+            d[i] = np.log10(domain[i])
+            d_sub[i] = np.log10(sub_domain[i])
+
+    # compute index ranges
+    index_ranges = np.zeros([num_dims, 2], dtype=int)
+    for i in range(num_dims):
+        dx = (d[i][1] - d[i][0]) / (dims[i] - 1)
+        lo_tmp = (d_sub[i][0] - d[i][0]) / dx
+        hi_tmp = (d_sub[i][1] - d[i][0]) / dx
+        if domain_rounding_type is None:
+            lo = int(np.around(lo_tmp))
+            if abs(lo - lo_tmp) / abs(lo_tmp) > EPS:
+                raise RuntimeError
+            hi = int(np.around(hi_tmp))
+            if abs(hi - hi_tmp) / abs(hi_tmp) > EPS:
+                raise RuntimeError
+        elif domain_rounding_type == 'expand':
+            lo = int(np.floor(lo_tmp))
+            hi = int(np.ceil(hi_tmp))
+        elif domain_rounding_type == 'contract':
+            lo = int(np.ceil(lo_tmp))
+            hi = int(np.floor(hi_tmp))
+        else:
+            raise ValueError(f"Unknown domain_rounding_type: {domain_rounding_type}")
+        index_ranges[i] = [lo, hi + 1]
+    mask = []
+    for i in range(len(index_ranges)):
+        mask.append(slice(index_ranges[i][0], index_ranges[i][1]))
+    mask = tuple(mask)
+    return mask
--- tests/test_nd_tree/test_masking.py
+++ tests/test_nd_tree/test_masking.py
@@ -1,7 +1,46 @@
 from nd_emulator.mask import create_mask
+import numpy as np
+import pytest
 
 
-def test_create_mask(dataset_2d):
+def test_create_mask():
     """
-    GIVEN:
+    GIVEN: the old domain, new domain, and dimensions of the data.
+    WHEN: create a mask function
+    THEN: function returns the correct subset of data
     """
+    domain = [[-1, 1], [-2, 2]]
+    dims = [21, 41]
+    # create dataset
+    x = np.linspace(domain[0][0], domain[0][1], dims[0])
+    y = np.linspace(domain[1][0], domain[1][1], dims[1])
+    X, Y = np.meshgrid(x, y)
+    Z = X+Y
+    # --- Test domain_rounding_type = None --- #
+    # create subdomain
+    sub_domain = [[-0.5, 0.5], [-1, 1]]
+    transforms = [None, None]
+    mask = create_mask(sub_domain, domain, dims, transforms, domain_rounding_type=None)
+    # check if mask worked
+    assert(np.all(Z[5:16, 10:31] == Z[mask]))
+    # make sure the exact gives the expected error
+    with pytest.raises(RuntimeError):
+        sub_domain = [[-0.499, 0.499], [-1, 1]]
+        transforms = [None, None]
+        mask = create_mask(sub_domain, domain, dims, transforms, domain_rounding_type=None)
+
+    # --- Test domain_rounding_type = expand --- #
+    # create subdomain
+    sub_domain = [[-0.499, 0.499], [-1, 1]]
+    transforms = [None, None]
+    mask = create_mask(sub_domain, domain, dims, transforms, domain_rounding_type='expand')
+    # check if mask worked
+    assert (np.all(Z[5:16, 10:31] == Z[mask]))
+
+    # --- Test domain_rounding_type = contract --- #
+    # create subdomain
+    sub_domain = [[-0.499, 0.499], [-1, 1]]
+    transforms = [None, None]
+    mask = create_mask(sub_domain, domain, dims, transforms, domain_rounding_type='contract')
+    # check if mask worked
+    assert (np.all(Z[6:15, 10:31] == Z[mask]))
