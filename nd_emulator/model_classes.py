c107736243fce3dd5ce59b3411fd136810783d65
Jared Carlson <JaredC.Scholar@gmail.com>
2021-05-19 10:31:12 -0400

    Finished implementing the nd-linear interpolation scheme with corresponding unit tests.

--- nd_emulator/model_classes.py
+++ nd_emulator/model_classes.py
@@ -1,5 +1,6 @@
 import numpy as np
 from itertools import permutations
+from numba import jit
 
 
 def fit_nd_linear_model(F, X):
@@ -7,26 +8,43 @@ def fit_nd_linear_model(F, X):
     :param F: (nd-array) function values
     :param X: (2 arrays) the coordinates of the lower and upper corner of the hypercube over which
         F is defined, ordered as [[x0_0, x1_0, ..., xN_0], [x0_N, ...xN_N]]
-    :return: (array) weights
+    :return: (array) weights [f_0...0, f_10...0, f_010..0, ..., f_1...1, x0_0..0, x1_0...0, ..., xN_0...0, x0_1...1,
+        ..., xN_1...1]
     """
     dims = F.ndim
-    weights = np.zeros([2**dims + 2*dims])
+    weights = np.zeros([2 ** dims + 2 * dims])
     # save corners of the hypercube
-    for i in range(2**dims):
-        index = tuple([int(d)*-1 for d in f'{i:0{dims}b}'[::-1]])
+    for i in range(2 ** dims):
+        index = tuple([int(d) * -1 for d in f'{i:0{dims}b}'[::-1]])
         weights[i] = F[index]
     # save two corner coordinates
-    weights[-2*dims:-dims] = X[0][:]
+    weights[-2 * dims:-dims] = X[0][:]
     weights[-dims:] = X[1][:]
     return weights
 
 
+@jit(nopython=True)
 def nd_linear_model(weights, x):
     """
     do an ND-linear interpolation at the point x given model weight values.
-    :param weights:
-    :param x:
+    https://math.stackexchange.com/a/1342377
+    :param weights:[f_0...0, f_10...0, f_010..0, ..., f_1...1, x0_0..0, x1_0...0, ..., xN_0...0, x0_1...1,
+        ..., xN_1...1]
+    :param x: (array) Point to interpolate at. We assume x and weights are in correct format
     :return:
     """
-
-    return
+    dims = len(x)
+    # transform x based on domain of hypercube
+    x = (x - weights[2 ** dims:2 ** dims + dims]) / (
+                weights[2 ** dims + dims:2 ** dims + 2 * dims] - weights[2 ** dims:2 ** dims + dims])
+    # iterate over each corner of hypercube
+    sol = 0
+    for i in range(2 ** dims):
+        w = 1
+        # get weight for corner
+        for j in range(dims):
+            # get binary index of current corner
+            bit = (i >> j) & 1
+            w *= 1 - x[j] if bit == 0 else x[j]
+        sol += weights[i] * w
+    return sol
--- tests/test_model_classes/test_nd_linear_model_class.py
+++ tests/test_model_classes/test_nd_linear_model_class.py
@@ -10,20 +10,19 @@ def test_nd_linear_model_class_fit_2d():
     :return:
     """
     DIMS = 2
-    EPS = 10**-15
+    EPS = 10 ** -15
     # build function values
     x0 = np.linspace(-1, 1, 10)
     x1 = np.linspace(-2, 2, 7)
     X0, X1 = np.meshgrid(x0, x1)
     # compute fucntion
-    F = X0*2 + X1
+    F = X0 * 2 + X1
     # get weights
     weights = fit_nd_linear_model(F, [[x0[0], x1[0]], [x0[-1], x1[-1]]])
     # compare to truth
     TRUE_WEIGHTS = np.array([-4, 0, 0, 4, -1, -2, 1, 2])
     for w_true, w in zip(TRUE_WEIGHTS, weights):
-        assert(abs(w-w_true) <= EPS)
-
+        assert (abs(w - w_true) <= EPS)
 
 
 def test_nd_linear_model_class_2d():
@@ -34,13 +33,54 @@ def test_nd_linear_model_class_2d():
     :return:
     """
     DIMS = 2
-    EPS = 10**-15
+    EPS = 10 ** -15
     # following the ordering [f_00, f_10, f_01, f_11, x0_00, x1_00, x0_11, x1_11]
     weights = np.array([-4, 0, 0, 4, -1, -2, 1, 2])
-    # input point
+    # input points
+
     x = np.array([0.5, -1.5])
     F_TRUE = -0.5
 
     f_interp = nd_linear_model(weights, x)
 
-    assert(abs(f_interp - F_TRUE) <= EPS)
+    assert (abs(f_interp - F_TRUE) <= EPS)
+
+
+def test_nd_linear_model_class_8d():
+    """
+    GIVEN: corner points of f(x) = sum(x_i*(i+1))
+    WHEN: interpolate random x's for f(x) = sum(x_i*(i+1))
+    THEN: interpolated values are exact to precision
+    :return:
+    """
+    EPS = 10 ** -10
+    DIMS = 8
+
+    # Create function that is 8d-linear
+    def f(x):
+        return sum([x[k] * (k + 1) for k in range(DIMS)])
+
+    # pick extreme corners
+    corner1 = [-1, 2, 4, 5, -1.5, -1, -1.1, -0.9]
+    corner2 = [1, 3, 6, 5.1, 7, 1, 2, 1]
+    # create weight vector
+    weights = np.zeros([2 ** DIMS + 2 * DIMS])
+    weights[2 ** DIMS:2 ** DIMS + DIMS] = corner1
+    weights[2 ** DIMS + DIMS:] = corner2
+    # evaluate function at each corner of the resulting hypercube
+    for i in range(2 ** DIMS):
+        x = []
+        for j in range(DIMS):
+            x.append(corner1[j] if (i >> j) & 1 == 0 else corner2[j])
+        weights[i] = f(np.array(x))
+
+    # evaluate f at random points
+    NUM_TEST_POINTS = 100
+    X = np.random.rand(NUM_TEST_POINTS, DIMS)
+    f_true = np.array([f(X[i, :]) for i in range(NUM_TEST_POINTS)])
+
+    # do interpolation at points
+    f_interp = np.array([nd_linear_model(weights, X[i, :]) for i in range(NUM_TEST_POINTS)])
+
+    # check that they are correct
+    assert (np.all(abs(f_interp - f_true) <= EPS))
