c3ffc45a0c70dfcea5a960328c0b59bce0455c5d
Jared Carlson <JaredC.Scholar@gmail.com>
2021-05-25 17:30:11 -0600

    Starting working on C++ version of the emulator.

---
+++ cpp_emulator/emulator.h
@@ -0,0 +1,28 @@
+//
+// Created by jared on 5/25/2021.
+//
+
+#ifndef CPP_EMULATOR_EMULATOR_H
+#define CPP_EMULATOR_EMULATOR_H
+#include "string"
+
+template <class encoding_int, class indexing_int>
+class Emulator {
+public:
+    Emulator(std::string filename){
+
+    }
+
+private:
+    int num_dim;
+    int num_model_classes;
+    int num_models;
+    int* offsets;
+    encoding_int * encoding_array;
+    indexing_int * indexing_array;
+    double ** mapping_arrays;
+};
+
+
+
+#endif //CPP_EMULATOR_EMULATOR_H
