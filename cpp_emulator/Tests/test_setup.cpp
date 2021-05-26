05559d6a4006dcf7784a3d86c799406060294701
Jared Carlson <JaredC.Scholar@gmail.com>
2021-05-26 13:56:03 -0600

    HDF5 is now working.

--- cpp_emulator/Tests/test_setup.cpp
+++ cpp_emulator/Tests/test_setup.cpp
@@ -4,6 +4,8 @@
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.h"
+#include "H5Cpp.h"
+#include <highfive/H5File.hpp>
 
 
 unsigned int Factorial( unsigned int number ) {
@@ -18,5 +20,25 @@ REQUIRE( Factorial(10) == 3628800 );
 }
 
 TEST_CASE("Test hdf5 read and write", "[HDF5]"){
+    using namespace HighFive;
+    // we create a new hdf5 file
+    File file("new_file.h5", File::ReadWrite | File::Create | File::Truncate);
 
+    std::vector<int> data(50, 1);
+    std::vector<int> data2(50, 2);
+
+    // let's create a dataset of native integer with the size of the vector 'data'
+    DataSet dataset = file.createDataSet<int>("./dataset_one.hdf5",  DataSpace::From(data));
+
+    // let's write our vector of int to the HDF5 dataset
+    dataset.write(data);
+
+    // read back
+    std::vector<int> result;
+    dataset.read(result);
+
+
+    // Test if it is the same
+    REQUIRE(data == result);
+    REQUIRE_FALSE(data2 == result);
 }
