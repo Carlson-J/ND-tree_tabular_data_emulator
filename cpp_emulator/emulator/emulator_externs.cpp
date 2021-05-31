cf88faf67f6055a581cded00295ff19208128f53
Jared Carlson <JaredC.Scholar@gmail.com>
2021-05-31 15:08:10 -0600

    Added extern wrappers for emulators. The emulator can be built, used for interpolated, and freed.

--- cpp_emulator/emulator/emulator_externs.cpp
+++ cpp_emulator/emulator/emulator_externs.cpp
@@ -4,26 +4,14 @@
 #include "emulator.h"
 #include "../Tests/saved_emulator_4d_constants.h"
 extern "C" {
-int print_me(const char* filename) {
-    std::cout << "testing" << std::endl;
-    std::printf("%s", filename);
-    std::cout << std::endl << std::endl;
-    return 6;}
-void send_array(int* ints, size_t num_ints){
-    for (int i = 0; i < num_ints; i++){
-        std::cout << ints[i] << std::endl;
-        ints[i]++;
+    Emulator<ND_TREE_EMULATOR_TYPE>* setup_emulator(const char* filename) {
+        std::cout << filename << std::endl;
+        return new Emulator<ND_TREE_EMULATOR_TYPE>(filename);
     }
-}
-Emulator<ND_TREE_EMULATOR_TYPE>* setup_emulator(const char* filename) {
-    std::cout << filename << std::endl;
-    return new Emulator<ND_TREE_EMULATOR_TYPE>(filename);
-}
-void interpolate(Emulator<ND_TREE_EMULATOR_TYPE>* emulator, double** points, size_t num_points, double* return_array) {
-//    for (int i = 0; i < num_points; i++){
-//        std::cout << return_array[i] << std::endl;
-//        return_array[i] = 99;
-//    }
-    emulator->interpolate(points, num_points, return_array);
-}
+    void interpolate(Emulator<ND_TREE_EMULATOR_TYPE>* emulator, double** points, size_t num_points, double* return_array) {
+        emulator->interpolate(points, num_points, return_array);
+    }
+    void free_emulator(Emulator<ND_TREE_EMULATOR_TYPE>* emulator){
+        free(emulator);
+    }
 }
