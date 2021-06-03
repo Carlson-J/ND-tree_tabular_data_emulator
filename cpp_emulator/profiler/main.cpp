8284d1d2ea549bc7c744125a69ea9eaac14dd726
Jared Carlson <JaredC.Scholar@gmail.com>
2021-06-03 17:49:19 -0600

    Changed cmake to keep debug symbols. Increase number of inputs.

--- cpp_emulator/profiler/main.cpp
+++ cpp_emulator/profiler/main.cpp
@@ -10,22 +10,24 @@ int main() {
     auto emulator = Emulator<ND_TREE_EMULATOR_TYPE>(table_location);
     // create test data
     // -- scan at a fixed x1
-    const size_t num_points = 200;
-    double x0[num_points];
-    double x1[num_points] = {10};
-    double* points[2] = {x0,x1};
-    for (size_t i = 0; i < num_points; i++){
-        x0[i] = double(i)/num_points;
+    const size_t num_points = 4000;
+    const size_t NUM_RUNS = 10;
+    for (size_t j = 0; j < NUM_RUNS; j++){
+        double x0[num_points];
+        double x1[num_points] = {10};
+        double* points[2] = {x0,x1};
+        for (size_t i = 0; i < num_points; i++){
+            x0[i] = double(i)/num_points;
+        }
+        double output[num_points] = {0};
+        // Start timer
+        auto start = std::chrono::steady_clock::now();
+        emulator.interpolate(points, num_points, output);
+        auto end = std::chrono::steady_clock::now();
+
+        auto diff  = end - start;
+        std::cout << "Time: " << std::chrono::duration <double, std::nano> (diff).count() << " ns" << std::endl;
     }
-    double output[num_points] = {0};
-
-    // Start timer
-    auto start = std::chrono::steady_clock::now();
-    emulator.interpolate(points, num_points, output);
-    auto end = std::chrono::steady_clock::now();
-
-    auto diff  = end - start;
-    std::cout << "Time: " << std::chrono::duration <double, std::nano> (diff).count() << " ns" << std::endl;
 //    for (size_t i = 0; i < num_points; i++){
 //        std::cout << output[i] << std::endl;
 //    }
