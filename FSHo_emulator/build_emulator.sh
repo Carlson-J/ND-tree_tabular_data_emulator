f92d102dc768a8605ff20337e333fc463f0f80b4
Jared Carlson <JaredC.Scholar@gmail.com>
2021-06-18 10:53:39 -0400

    Added files for creating emulators for the FSHo EOS and for computing the tables error

---
+++ FSHo_emulator/build_emulator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=12:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=1   # 1 processor core(s) per node 
+#SBATCH --mem=20G   # maximum memory per node
+#SBATCH --job-name="build_emulator"
+#SBATCH --output="build_emulator.out" # job standard output file (%j replaced by job id)
+
+# setup env
+source ../../../load_env
+conda activate nd_emulator 
+export PYTHONPATH=${PYTHONPATH}:/mnt/home/carls502/ANL/ND-tree_tabular_data_emulator/
+
+# do computation
+python3 build_FSHo_emulator.py
