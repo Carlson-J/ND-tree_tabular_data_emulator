e44324a4173e172e47340e9908e0fb0c4fb4e587
Jared Carlson <JaredC.Scholar@gmail.com>
2021-07-22 17:15:24 -0400

    WIP: Making mapping function builder.

--- cpp_emulator/PMHF_mapping/build_mapping.cpp
+++ cpp_emulator/PMHF_mapping/build_mapping.cpp
@@ -9,13 +9,14 @@
 
 int main() {
     using namespace pthash;
-    std::string file_location = "./testing_v2_table.hdf5";
-    size_t num_points = 4;
-    std::vector<uint16_t> indexing(num_points);
+    std::string file_location = "./test_v2_sparse_table.hdf5";
+    size_t num_points = 2555297;
+    std::vector<unsigned long> indexing(num_points);
     std::vector<double> node_values(num_points);
     std::vector<double> new_node_values(num_points);
+    std::vector<double> new_node_values_testing(num_points);
     // Load mapping data
-    HighFive::File file(file_location, HighFive::File::ReadOnly);
+    HighFive::File file(file_location, HighFive::File::ReadWrite);
     HighFive::Group mapping_group = file.getGroup("mapping");
     HighFive::DataSet dataset = mapping_group.getDataSet("indexing");
     dataset.template read(indexing);
@@ -24,14 +25,14 @@ int main() {
 
     /* Set up a build configuration. */
     build_configuration config;
-    config.c = 6.0;
-    config.alpha = 0.94;
+    config.c = 2.5;//6.0;
+    config.alpha = 0.99;
     config.minimal_output = true;  // mphf
     config.verbose_output = true;
-    config.num_partitions = 3;
+//    config.num_partitions = 3;
     /* Declare the PTHash function. */
     typedef single_phf<murmurhash2_64,         // base hasher
-            dictionary_dictionary,  // encoder type
+            compact_compact,  // encoder type
             true                    // minimal
     >
             pthash_type;
@@ -64,15 +65,15 @@ int main() {
     if (check(indexing.begin(), f)) std::cout << "EVERYTHING OK!" << std::endl;
 
     /* Now rearrange data to be in the correct place */
-    for (uint64_t i = 0; i != indexing.size(); ++i) {
+    for (uint64_t i = 0; i != num_points; ++i) {
         auto new_index = f(indexing[i]);
-        std::cout << "f(" << (int)indexing[i] << ") = " << f(indexing[i]) << ", storing " << node_values[i] << '\n';
+//        std::cout << "f(" << (int)indexing[i] << ") = " << f(indexing[i]) << ", storing " << node_values[i] << '\n';
         new_node_values[new_index] = node_values[i];
     }
 
     // Print out new array
     std::cout << "\nPrint out new array:\n";
-    for (uint64_t i = 0; i != indexing.size(); ++i) {
+    for (uint64_t i = 0; i != 10; ++i) {
         std::cout << "i " << i << ": " << new_node_values[i] << '\n';
     }
 
@@ -86,10 +87,37 @@ int main() {
         pthash_type other;
         essentials::load(other, output_filename.c_str());
         for (uint64_t i = 0; i != 10; ++i) {
-            std::cout << "f(" << (int)indexing[i] << ") = " << other(indexing[i]) << '\n';
+            std::cout << "f(" << static_cast<int>(indexing[i]) << ") = " << other(indexing[i]) << '\n';
             assert(f(indexing[i]) == other(indexing[i]));
         }
     }
+
+//    // Save new order of values for mapping array
+//    for (uint64_t i = 0; i != num_points; ++i) {
+//        if (i == 0){
+//            std::cout << "val at i0: " << node_values[f(indexing[i])] << std::endl;
+//        }
+//        new_node_values[i] = node_values[f(indexing[i])];
+//    }
+//    mapping_group.createDataSet<double>("node_values_encoded", HighFive::DataSpace::From(new_node_values));
+    dataset = mapping_group.getDataSet("node_values_encoded");
+    dataset.write(new_node_values);
+
+    // Test to make sure it worked.
+    dataset.template read(new_node_values_testing);
+    for (unsigned int i = 0; i != num_points; ++i) {
+        if (new_node_values[i] != new_node_values_testing[i]){
+            std::cout << "Error saving values!" << std::endl;
+            return -1;
+        }
+        if (node_values[i] != new_node_values_testing[f(indexing[i])]){
+            std::cout << "i:" << i << "->" << indexing[i] << ", n1:" << node_values[i] << ", n2:" << new_node_values_testing[f(indexing[i])]
+             << " = " << "N[" << f(indexing[i]) << "]" << std::endl;
+            std::cout << "Error mapping values!" << std::endl;
+            return -1;
+        }
+    }
+    std::cout << "Test Passed!" << std::endl;
 
     return 0;
 }
--- cpp_emulator/emulator/table_params.h
+++ cpp_emulator/emulator/table_params.h
@@ -2,7 +2,7 @@
 #undef ND_TREE_EMULATOR_NAME_SETUP
 #undef ND_TREE_EMULATOR_NAME_INTERPOLATE
 #undef ND_TREE_EMULATOR_NAME_FREE
-#define ND_TREE_EMULATOR_TYPE unsigned short, 1, 2, 8385, 8192
-#define ND_TREE_EMULATOR_NAME_SETUP testing_v2_emulator_setup
-#define ND_TREE_EMULATOR_NAME_INTERPOLATE testing_v2_emulator_interpolate
-#define ND_TREE_EMULATOR_NAME_FREE testing_v2_emulator_free
+#define ND_TREE_EMULATOR_TYPE unsigned long, 1, 3, 2555297, 8388608
+#define ND_TREE_EMULATOR_NAME_SETUP test_v2_sparse_emulator_setup
+#define ND_TREE_EMULATOR_NAME_INTERPOLATE test_v2_sparse_emulator_interpolate
+#define ND_TREE_EMULATOR_NAME_FREE test_v2_sparse_emulator_free
