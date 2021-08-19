75acc855db4710c6cd239432c91f8ca0b84a469b
Jared Carlson <JaredC.Scholar@gmail.com>
2021-08-19 16:54:28 -0400

    WIP: working on getting serial version working correctly with derivative return

--- SRO_emulator/build_SRO_emulator.py
+++ SRO_emulator/build_SRO_emulator.py
@@ -2,7 +2,6 @@ import os
 import sys
 import matplotlib.pyplot as plt
 
-from data_loading_functions.load_test_data import load_test_data
 from nd_emulator import build_emulator, make_cpp_emulator
 from data_loading_functions import load_SRO_EOS
 import numpy as np
@@ -18,6 +17,8 @@ if __name__ == "__main__":
     EOS_file = "../../tables/SRO_training_rho1025_temp513_ye129_gitM6eba730_20210624.h5"
     vars, domain = load_SRO_EOS(EOS_file)
     spacing = ['linear', 'linear', 'linear']      # We will do the transform ahead of time.
+
+    skip_table_creation = True
 
     relative_error_dict = {
         'Abar': True,
@@ -73,18 +74,19 @@ if __name__ == "__main__":
         max_test_points = 100       # The max number of points to eval in a cell when estimating the error
         relative_error = relative_error_dict[var]      # Whether or not the error threshold is absolute or relative error
 
-        # create the emulator (should not need to modify this)
-        emulator = build_emulator(vars[var], max_depth, domain, spacing, error_threshold, model_classes,
-                                max_test_points=max_test_points, relative_error=relative_error,
-                                expand_index_domain=True, error_type="max")
+        if not skip_table_creation:
+            # create the emulator (should not need to modify this)
+            emulator = build_emulator(vars[var], max_depth, domain, spacing, error_threshold, model_classes,
+                                    max_test_points=max_test_points, relative_error=relative_error,
+                                    expand_index_domain=True, error_type="max")
 
-        # ------- No not edit below here ------- #
-        # create folder to save files in
-        if not os.path.isdir(save_directory):
-            os.makedirs(save_directory)
+            # ------- No not edit below here ------- #
+            # create folder to save files in
+            if not os.path.isdir(save_directory):
+                os.makedirs(save_directory)
 
-        # save compact emulator
-        emulator.save(save_directory, emulator_name)
+            # save compact emulator
+            emulator.save(save_directory, emulator_name)
         
         make_cpp_emulator(save_directory, emulator_name, cpp_source_dir)
 
