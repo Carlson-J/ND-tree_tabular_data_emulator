5e679521ed57e565efd20ca51536786cc18534f8
Jared Carlson <JaredC.Scholar@gmail.com>
2021-08-25 11:32:35 -0600

    Removed unnneeded files.

--- cpp_emulator/regular_grid_nd_linear/compare_nd_linear.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by jared on 6/9/2021.
-//
-
-extern "C" {
-    void intp3d_many(double* x, double* y, double* z, double* f, int kt, double* ft, int nx, int ny, int nz, int nvars,
-                     double* xt, double* yt, double* zt);
-}
